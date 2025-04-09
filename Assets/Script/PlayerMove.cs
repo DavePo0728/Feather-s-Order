6374ca35315d5b434039e703e4957ac49cb3bd29
ccesfugh <cces45745@gmail.com>
2025-04-10 04:33:43 +0800

    0410wavedata

--- Assets/Script/PlayerMove.cs
+++ Assets/Script/PlayerMove.cs
@@ -86,7 +86,7 @@ public class PlayerMove : MonoBehaviour
     }
     public void GetDash(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && playerSlashAttack.isFallBack == false)
         {
             if (canDash/*&&currentEnergy>=20&&!isOutBurst*/)
             {
