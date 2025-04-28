175654969dc872f4b1a11b7b00e694eee5d76b88
Asuisui <tomsowo@gmail.com>
2025-04-28 14:22:35 +0800

    Asuisui #042801

---
+++ Assets/Enemy/Boss/BossController.cs
@@ -0,0 +1,316 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+	[Header("一般控制滑條")]
+	[Range(0f, 1f)] public float trigger1 = 0;
+	[Range(0f, 1f)] public float trigger2 = 0;
+	[Range(0f, 1f)] public float trigger3 = 0;
+	[Range(0f, 1f)] public float trigger4 = 0;
+	[Range(0f, 1f)] public float specialTrigger = 0;
+	[Range(0f, 1f)] public float newTrigger = 0;
+	[Range(0f, 1f)] public float outTrigger = 0;
+
+	[Header("觸手葉子滑條 (四個方向)")]
+	[Range(0f, 1f)] public float tentacleLeftUptrigger = 0;
+	[Range(0f, 1f)] public float tentacleRightUptrigger = 0;
+	[Range(0f, 1f)] public float tentacleLeftDowntrigger = 0;
+	[Range(0f, 1f)] public float tentacleRightDowntrigger = 0;
+
+	[Header("觸手花瓣滑條 (四個方向)")]
+	[Range(0f, 1f)] public float tentacleflowerLeftUptrigger = 0;
+	[Range(0f, 1f)] public float tentacleflowerRightUptrigger = 0;
+	[Range(0f, 1f)] public float tentacleflowerLeftDowntrigger = 0;
+	[Range(0f, 1f)] public float tentacleflowerRightDowntrigger = 0;
+
+	[Header("一般眼睛控制群組")]
+	public List<Transform> targetGroup1 = new List<Transform>();
+	public List<Transform> targetGroup2 = new List<Transform>();
+	public List<Transform> targetGroup3 = new List<Transform>();
+	public List<Transform> targetGroup4 = new List<Transform>();
+
+	[Header("中央眼睛群組")]
+	public List<Transform> downList = new List<Transform>();
+	public Transform target;
+
+	[Header("眼睛花瓣旋轉控制")]
+	public List<Transform> rotationGroup1 = new List<Transform>();
+	public List<Transform> rotationGroup2 = new List<Transform>();
+	public List<Transform> rotationGroup3 = new List<Transform>();
+
+	public float maxRotation1 = 90f;
+	public float maxRotation2 = 45f;
+	public float maxRotation3 = 60f;
+
+	[Header("眼睛朝向設定")]
+	public Transform Look_Target;
+	public List<Transform> EyesGroup = new List<Transform>();
+
+	[Header("觸手葉片群組 (每個方向5個List)")]
+	public List<Transform> tentacleLeftUpGroup1 = new List<Transform>();
+	public List<Transform> tentacleLeftUpGroup2 = new List<Transform>();
+	public List<Transform> tentacleLeftUpGroup3 = new List<Transform>();
+	public List<Transform> tentacleLeftUpGroup4 = new List<Transform>();
+	public List<Transform> tentacleLeftUpGroup5 = new List<Transform>();
+
+	public List<Transform> tentacleRightUpGroup1 = new List<Transform>();
+	public List<Transform> tentacleRightUpGroup2 = new List<Transform>();
+	public List<Transform> tentacleRightUpGroup3 = new List<Transform>();
+	public List<Transform> tentacleRightUpGroup4 = new List<Transform>();
+	public List<Transform> tentacleRightUpGroup5 = new List<Transform>();
+
+	public List<Transform> tentacleLeftDownGroup1 = new List<Transform>();
+	public List<Transform> tentacleLeftDownGroup2 = new List<Transform>();
+	public List<Transform> tentacleLeftDownGroup3 = new List<Transform>();
+	public List<Transform> tentacleLeftDownGroup4 = new List<Transform>();
+	public List<Transform> tentacleLeftDownGroup5 = new List<Transform>();
+
+	public List<Transform> tentacleRightDownGroup1 = new List<Transform>();
+	public List<Transform> tentacleRightDownGroup2 = new List<Transform>();
+	public List<Transform> tentacleRightDownGroup3 = new List<Transform>();
+	public List<Transform> tentacleRightDownGroup4 = new List<Transform>();
+	public List<Transform> tentacleRightDownGroup5 = new List<Transform>();
+
+	[Header("觸手葉片最大旋轉 (每個5個最大值)")]
+	public float maxLeafRotation1 = 90f;
+	public float maxLeafRotation2 = 80f;
+	public float maxLeafRotation3 = 70f;
+	public float maxLeafRotation4 = 60f;
+	public float maxLeafRotation5 = 50f;
+
+	[Header("觸手花瓣群組 (每個方向3個List)")]
+	public List<Transform> tentacleflowerLeftUpGroup1 = new List<Transform>();
+	public List<Transform> tentacleflowerLeftUpGroup2 = new List<Transform>();
+	public List<Transform> tentacleflowerLeftUpGroup3 = new List<Transform>();
+
+	public List<Transform> tentacleflowerRightUpGroup1 = new List<Transform>();
+	public List<Transform> tentacleflowerRightUpGroup2 = new List<Transform>();
+	public List<Transform> tentacleflowerRightUpGroup3 = new List<Transform>();
+
+	public List<Transform> tentacleflowerLeftDownGroup1 = new List<Transform>();
+	public List<Transform> tentacleflowerLeftDownGroup2 = new List<Transform>();
+	public List<Transform> tentacleflowerLeftDownGroup3 = new List<Transform>();
+
+	public List<Transform> tentacleflowerRightDownGroup1 = new List<Transform>();
+	public List<Transform> tentacleflowerRightDownGroup2 = new List<Transform>();
+	public List<Transform> tentacleflowerRightDownGroup3 = new List<Transform>();
+
+	[Header("觸手花瓣最大旋轉 (每個3個最大值)")]
+	public float maxFlowerRotation1 = 90f;
+	public float maxFlowerRotation2 = 60f;
+	public float maxFlowerRotation3 = 30f;
+	private Dictionary<Transform, Quaternion> originalRotations = new Dictionary<Transform, Quaternion>();
+
+	private void Start()
+	{
+		// 記錄所有物件的初始旋轉
+		CacheOriginalRotations();
+	}
+
+	private void CacheOriginalRotations()
+	{
+		// 把所有 group 的物件都存起來
+		List<List<Transform>> allGroups = new List<List<Transform>>
+		{
+            // 觸手葉片
+            tentacleLeftUpGroup1, tentacleLeftUpGroup2, tentacleLeftUpGroup3, tentacleLeftUpGroup4, tentacleLeftUpGroup5,
+			tentacleRightUpGroup1, tentacleRightUpGroup2, tentacleRightUpGroup3, tentacleRightUpGroup4, tentacleRightUpGroup5,
+			tentacleLeftDownGroup1, tentacleLeftDownGroup2, tentacleLeftDownGroup3, tentacleLeftDownGroup4, tentacleLeftDownGroup5,
+			tentacleRightDownGroup1, tentacleRightDownGroup2, tentacleRightDownGroup3, tentacleRightDownGroup4, tentacleRightDownGroup5,
+
+            // 觸手花瓣
+            tentacleflowerLeftUpGroup1, tentacleflowerLeftUpGroup2, tentacleflowerLeftUpGroup3,
+			tentacleflowerRightUpGroup1, tentacleflowerRightUpGroup2, tentacleflowerRightUpGroup3,
+			tentacleflowerLeftDownGroup1, tentacleflowerLeftDownGroup2, tentacleflowerLeftDownGroup3,
+			tentacleflowerRightDownGroup1, tentacleflowerRightDownGroup2, tentacleflowerRightDownGroup3
+		};
+
+		foreach (var group in allGroups)
+		{
+			foreach (var t in group)
+			{
+				if (t != null && !originalRotations.ContainsKey(t))
+				{
+					originalRotations.Add(t, t.localRotation);
+				}
+			}
+		}
+	}
+
+	private void Update()
+	{
+		UpdateGroup(targetGroup1, trigger1);
+		UpdateGroup(targetGroup2, trigger2);
+		UpdateGroup(targetGroup3, trigger3);
+		UpdateGroup(targetGroup4, trigger4);
+
+		UpdateDown();
+		UpdateTarget();
+		UpdateEyesGroupPosition();
+		//UpdateTentacleLeaf();
+		//UpdateTentacleFlower();
+		UpdateRotation(rotationGroup1, outTrigger, maxRotation1, Vector3.forward);
+		UpdateRotation(rotationGroup2, outTrigger, maxRotation2, Vector3.forward);
+		UpdateRotation(rotationGroup3, outTrigger, maxRotation3, Vector3.forward);
+
+		// 左上
+		UpdateRotation(tentacleflowerLeftUpGroup1, tentacleflowerLeftUptrigger, maxFlowerRotation1, Vector3.forward);
+		UpdateRotation(tentacleflowerLeftUpGroup2, tentacleflowerLeftUptrigger, maxFlowerRotation2, Vector3.forward);
+		UpdateRotation(tentacleflowerLeftUpGroup3, tentacleflowerLeftUptrigger, maxFlowerRotation3, Vector3.forward);
+
+		UpdateRotation(tentacleLeftUpGroup1, tentacleLeftUptrigger, maxLeafRotation1, Vector3.forward);
+		UpdateRotation(tentacleLeftUpGroup2, tentacleLeftUptrigger, maxLeafRotation2, Vector3.forward);
+		UpdateRotation(tentacleLeftUpGroup3, tentacleLeftUptrigger, maxLeafRotation3, Vector3.forward);
+		UpdateRotation(tentacleLeftUpGroup4, tentacleLeftUptrigger, maxLeafRotation4, Vector3.forward);
+		UpdateRotation(tentacleLeftUpGroup5, tentacleLeftUptrigger, maxLeafRotation5, Vector3.forward);
+
+		// 右上
+		UpdateRotation(tentacleflowerRightUpGroup1, tentacleflowerRightUptrigger, maxFlowerRotation1, Vector3.forward);
+		UpdateRotation(tentacleflowerRightUpGroup2, tentacleflowerRightUptrigger, maxFlowerRotation2, Vector3.forward);
+		UpdateRotation(tentacleflowerRightUpGroup3, tentacleflowerRightUptrigger, maxFlowerRotation3, Vector3.forward);
+
+		UpdateRotation(tentacleRightUpGroup1, tentacleRightUptrigger, maxLeafRotation1, Vector3.forward);
+		UpdateRotation(tentacleRightUpGroup2, tentacleRightUptrigger, maxLeafRotation2, Vector3.forward);
+		UpdateRotation(tentacleRightUpGroup3, tentacleRightUptrigger, maxLeafRotation3, Vector3.forward);
+		UpdateRotation(tentacleRightUpGroup4, tentacleRightUptrigger, maxLeafRotation4, Vector3.forward);
+		UpdateRotation(tentacleRightUpGroup5, tentacleRightUptrigger, maxLeafRotation5, Vector3.forward);
+
+		// 左下
+		UpdateRotation(tentacleflowerLeftDownGroup1, tentacleflowerLeftDowntrigger, maxFlowerRotation1, Vector3.forward);
+		UpdateRotation(tentacleflowerLeftDownGroup2, tentacleflowerLeftDowntrigger, maxFlowerRotation2, Vector3.forward);
+		UpdateRotation(tentacleflowerLeftDownGroup3, tentacleflowerLeftDowntrigger, maxFlowerRotation3, Vector3.forward);
+
+		UpdateRotation(tentacleLeftDownGroup1, tentacleLeftDowntrigger, maxLeafRotation1, Vector3.forward);
+		UpdateRotation(tentacleLeftDownGroup2, tentacleLeftDowntrigger, maxLeafRotation2, Vector3.forward);
+		UpdateRotation(tentacleLeftDownGroup3, tentacleLeftDowntrigger, maxLeafRotation3, Vector3.forward);
+		UpdateRotation(tentacleLeftDownGroup4, tentacleLeftDowntrigger, maxLeafRotation4, Vector3.forward);
+		UpdateRotation(tentacleLeftDownGroup5, tentacleLeftDowntrigger, maxLeafRotation5, Vector3.forward);
+
+		// 右下
+		UpdateRotation(tentacleflowerRightDownGroup1, tentacleflowerRightDowntrigger, maxFlowerRotation1, Vector3.forward);
+		UpdateRotation(tentacleflowerRightDownGroup2, tentacleflowerRightDowntrigger, maxFlowerRotation2, Vector3.forward);
+		UpdateRotation(tentacleflowerRightDownGroup3, tentacleflowerRightDowntrigger, maxFlowerRotation3, Vector3.forward);
+
+		UpdateRotation(tentacleRightDownGroup1, tentacleRightDowntrigger, maxLeafRotation1, Vector3.forward);
+		UpdateRotation(tentacleRightDownGroup2, tentacleRightDowntrigger, maxLeafRotation2, Vector3.forward);
+		UpdateRotation(tentacleRightDownGroup3, tentacleRightDowntrigger, maxLeafRotation3, Vector3.forward);
+		UpdateRotation(tentacleRightDownGroup4, tentacleRightDowntrigger, maxLeafRotation4, Vector3.forward);
+		UpdateRotation(tentacleRightDownGroup5, tentacleRightDowntrigger, maxLeafRotation5, Vector3.forward);
+
+	}
+
+	private void UpdateTentacleLeaf()
+	{
+		RotateGroup(tentacleLeftUptrigger, tentacleLeftUpGroup1, maxLeafRotation1);
+		RotateGroup(tentacleLeftUptrigger, tentacleLeftUpGroup2, maxLeafRotation2);
+		RotateGroup(tentacleLeftUptrigger, tentacleLeftUpGroup3, maxLeafRotation3);
+		RotateGroup(tentacleLeftUptrigger, tentacleLeftUpGroup4, maxLeafRotation4);
+		RotateGroup(tentacleLeftUptrigger, tentacleLeftUpGroup5, maxLeafRotation5);
+
+		RotateGroup(tentacleRightUptrigger, tentacleRightUpGroup1, maxLeafRotation1);
+		RotateGroup(tentacleRightUptrigger, tentacleRightUpGroup2, maxLeafRotation2);
+		RotateGroup(tentacleRightUptrigger, tentacleRightUpGroup3, maxLeafRotation3);
+		RotateGroup(tentacleRightUptrigger, tentacleRightUpGroup4, maxLeafRotation4);
+		RotateGroup(tentacleRightUptrigger, tentacleRightUpGroup5, maxLeafRotation5);
+
+		RotateGroup(tentacleLeftDowntrigger, tentacleLeftDownGroup1, maxLeafRotation1);
+		RotateGroup(tentacleLeftDowntrigger, tentacleLeftDownGroup2, maxLeafRotation2);
+		RotateGroup(tentacleLeftDowntrigger, tentacleLeftDownGroup3, maxLeafRotation3);
+		RotateGroup(tentacleLeftDowntrigger, tentacleLeftDownGroup4, maxLeafRotation4);
+		RotateGroup(tentacleLeftDowntrigger, tentacleLeftDownGroup5, maxLeafRotation5);
+
+		RotateGroup(tentacleRightDowntrigger, tentacleRightDownGroup1, maxLeafRotation1);
+		RotateGroup(tentacleRightDowntrigger, tentacleRightDownGroup2, maxLeafRotation2);
+		RotateGroup(tentacleRightDowntrigger, tentacleRightDownGroup3, maxLeafRotation3);
+		RotateGroup(tentacleRightDowntrigger, tentacleRightDownGroup4, maxLeafRotation4);
+		RotateGroup(tentacleRightDowntrigger, tentacleRightDownGroup5, maxLeafRotation5);
+	}
+
+	private void UpdateTentacleFlower()
+	{
+		RotateGroup(tentacleflowerLeftUptrigger, tentacleflowerLeftUpGroup1, maxFlowerRotation1);
+		RotateGroup(tentacleflowerLeftUptrigger, tentacleflowerLeftUpGroup2, maxFlowerRotation2);
+		RotateGroup(tentacleflowerLeftUptrigger, tentacleflowerLeftUpGroup3, maxFlowerRotation3);
+
+		RotateGroup(tentacleflowerRightUptrigger, tentacleflowerRightUpGroup1, maxFlowerRotation1);
+		RotateGroup(tentacleflowerRightUptrigger, tentacleflowerRightUpGroup2, maxFlowerRotation2);
+		RotateGroup(tentacleflowerRightUptrigger, tentacleflowerRightUpGroup3, maxFlowerRotation3);
+
+		RotateGroup(tentacleflowerLeftDowntrigger, tentacleflowerLeftDownGroup1, maxFlowerRotation1);
+		RotateGroup(tentacleflowerLeftDowntrigger, tentacleflowerLeftDownGroup2, maxFlowerRotation2);
+		RotateGroup(tentacleflowerLeftDowntrigger, tentacleflowerLeftDownGroup3, maxFlowerRotation3);
+
+		RotateGroup(tentacleflowerRightDowntrigger, tentacleflowerRightDownGroup1, maxFlowerRotation1);
+		RotateGroup(tentacleflowerRightDowntrigger, tentacleflowerRightDownGroup2, maxFlowerRotation2);
+		RotateGroup(tentacleflowerRightDowntrigger, tentacleflowerRightDownGroup3, maxFlowerRotation3);
+	}
+	void UpdateGroup(List<Transform> targets, float trigger)
+	{
+		float scaleValue = 1f - trigger;
+
+		foreach (Transform t in targets)
+		{
+			t.localScale = new Vector3(scaleValue, scaleValue, scaleValue);
+		}
+	}
+	void UpdateDown()
+	{
+		float downScale = Mathf.Lerp(1f, 0.05f, specialTrigger);
+
+		foreach (Transform t in downList)
+		{
+			t.localScale = new Vector3(downScale, downScale, downScale);
+		}
+	}
+	void UpdateTarget()
+	{
+		if (target != null)
+		{
+			float targetScale = 1f - newTrigger;
+			target.localScale = new Vector3(targetScale, targetScale, targetScale);
+		}
+	}
+	private void RotateGroup(float trigger, List<Transform> group, float maxRotation)
+	{
+		foreach (Transform t in group)
+		{
+			if (t != null && originalRotations.ContainsKey(t))
+			{
+				Quaternion baseRotation = originalRotations[t];
+				Quaternion addRotation = Quaternion.Euler(maxRotation * trigger, 0f, 0f);
+				t.localRotation = baseRotation * addRotation;
+			}
+		}
+	}
+	void UpdateRotation(List<Transform> targets, float trigger, float maxRotation, Vector3 axis)
+	{
+		foreach (Transform t in targets)
+		{
+			if (t != null && originalRotations.ContainsKey(t))
+			{
+				Quaternion original = originalRotations[t];
+				float addRotation = Mathf.Lerp(0f, maxRotation, trigger);
+
+				// 按照指定軸向產生旋轉
+				Quaternion deltaRotation = Quaternion.Euler(axis.normalized * addRotation);
+
+				// 套用原本的旋轉 + 累加旋轉
+				t.localRotation = original * deltaRotation;
+			}
+		}
+	}
+	void UpdateEyesGroupPosition()
+	{
+		foreach (Transform eye in EyesGroup)
+		{
+			if (eye != null && eye.parent != null && Look_Target != null)
+			{
+				Vector3 localPos = eye.parent.InverseTransformPoint(Look_Target.position);
+				eye.localPosition = localPos;
+			}
+		}
+	}
+}
+
---
+++ Assets/Enemy/Boss/BossControllerEditor.cs
@@ -0,0 +1,207 @@
+﻿//using UnityEditor;
+//using UnityEngine;
+
+//[CustomEditor(typeof(BossController))]
+//public class BossControllerEditor : Editor
+//{
+//	SerializedProperty trigger1, trigger2, trigger3, trigger4;
+//	SerializedProperty specialTrigger, newTrigger, outTrigger;
+//	SerializedProperty targetGroup1, targetGroup2, targetGroup3, targetGroup4;
+//	SerializedProperty downList, target;
+//	SerializedProperty rotationGroup1, rotationGroup2, rotationGroup3;
+//	SerializedProperty maxRotation1, maxRotation2, maxRotation3;
+//	SerializedProperty Look_Target, EyesGroup;
+
+//	// 觸手葉片與花瓣用的
+//	SerializedProperty tentacleLeftUptrigger, tentacleRightUptrigger, tentacleLeftDowntrigger, tentacleRightDowntrigger;
+//	SerializedProperty tentacleflowerLeftUptrigger, tentacleflowerRightUptrigger, tentacleflowerLeftDowntrigger, tentacleflowerRightDowntrigger;
+
+//	SerializedProperty tentacleLeftUpGroup1, tentacleLeftUpGroup2, tentacleLeftUpGroup3, tentacleLeftUpGroup4, tentacleLeftUpGroup5;
+//	SerializedProperty tentacleRightUpGroup1, tentacleRightUpGroup2, tentacleRightUpGroup3, tentacleRightUpGroup4, tentacleRightUpGroup5;
+//	SerializedProperty tentacleLeftDownGroup1, tentacleLeftDownGroup2, tentacleLeftDownGroup3, tentacleLeftDownGroup4, tentacleLeftDownGroup5;
+//	SerializedProperty tentacleRightDownGroup1, tentacleRightDownGroup2, tentacleRightDownGroup3, tentacleRightDownGroup4, tentacleRightDownGroup5;
+
+//	SerializedProperty maxLeafRotation1, maxLeafRotation2, maxLeafRotation3, maxLeafRotation4, maxLeafRotation5;
+
+//	SerializedProperty tentacleflowerLeftUpGroup1, tentacleflowerLeftUpGroup2, tentacleflowerLeftUpGroup3;
+//	SerializedProperty tentacleflowerRightUpGroup1, tentacleflowerRightUpGroup2, tentacleflowerRightUpGroup3;
+//	SerializedProperty tentacleflowerLeftDownGroup1, tentacleflowerLeftDownGroup2, tentacleflowerLeftDownGroup3;
+//	SerializedProperty tentacleflowerRightDownGroup1, tentacleflowerRightDownGroup2, tentacleflowerRightDownGroup3;
+
+//	SerializedProperty maxFlowerRotation1, maxFlowerRotation2, maxFlowerRotation3;
+//	void OnEnable()
+//	{
+//		trigger1 = serializedObject.FindProperty("trigger1");
+//		trigger2 = serializedObject.FindProperty("trigger2");
+//		trigger3 = serializedObject.FindProperty("trigger3");
+//		trigger4 = serializedObject.FindProperty("trigger4");
+//		specialTrigger = serializedObject.FindProperty("specialTrigger");
+//		newTrigger = serializedObject.FindProperty("newTrigger");
+//		outTrigger = serializedObject.FindProperty("outTrigger");
+
+//		targetGroup1 = serializedObject.FindProperty("targetGroup1");
+//		targetGroup2 = serializedObject.FindProperty("targetGroup2");
+//		targetGroup3 = serializedObject.FindProperty("targetGroup3");
+//		targetGroup4 = serializedObject.FindProperty("targetGroup4");
+
+//		downList = serializedObject.FindProperty("downList");
+//		target = serializedObject.FindProperty("target");
+
+//		rotationGroup1 = serializedObject.FindProperty("rotationGroup1");
+//		rotationGroup2 = serializedObject.FindProperty("rotationGroup2");
+//		rotationGroup3 = serializedObject.FindProperty("rotationGroup3");
+
+//		maxRotation1 = serializedObject.FindProperty("maxRotation1");
+//		maxRotation2 = serializedObject.FindProperty("maxRotation2");
+//		maxRotation3 = serializedObject.FindProperty("maxRotation3");
+
+//		Look_Target = serializedObject.FindProperty("Look_Target");
+//		EyesGroup = serializedObject.FindProperty("EyesGroup");
+
+//		// 葉片 花瓣 滑條
+//		tentacleLeftUptrigger = serializedObject.FindProperty("tentacleLeftUptrigger");
+//		tentacleRightUptrigger = serializedObject.FindProperty("tentacleRightUptrigger");
+//		tentacleLeftDowntrigger = serializedObject.FindProperty("tentacleLeftDowntrigger");
+//		tentacleRightDowntrigger = serializedObject.FindProperty("tentacleRightDowntrigger");
+
+//		tentacleflowerLeftUptrigger = serializedObject.FindProperty("tentacleflowerLeftUptrigger");
+//		tentacleflowerRightUptrigger = serializedObject.FindProperty("tentacleflowerRightUptrigger");
+//		tentacleflowerLeftDowntrigger = serializedObject.FindProperty("tentacleflowerLeftDowntrigger");
+//		tentacleflowerRightDowntrigger = serializedObject.FindProperty("tentacleflowerRightDowntrigger");
+
+//		// 葉片 List
+//		tentacleLeftUpGroup1 = serializedObject.FindProperty("tentacleLeftUpGroup1");
+//		tentacleLeftUpGroup2 = serializedObject.FindProperty("tentacleLeftUpGroup2");
+//		tentacleLeftUpGroup3 = serializedObject.FindProperty("tentacleLeftUpGroup3");
+//		tentacleLeftUpGroup4 = serializedObject.FindProperty("tentacleLeftUpGroup4");
+//		tentacleLeftUpGroup5 = serializedObject.FindProperty("tentacleLeftUpGroup5");
+
+//		tentacleRightUpGroup1 = serializedObject.FindProperty("tentacleRightUpGroup1");
+//		tentacleRightUpGroup2 = serializedObject.FindProperty("tentacleRightUpGroup2");
+//		tentacleRightUpGroup3 = serializedObject.FindProperty("tentacleRightUpGroup3");
+//		tentacleRightUpGroup4 = serializedObject.FindProperty("tentacleRightUpGroup4");
+//		tentacleRightUpGroup5 = serializedObject.FindProperty("tentacleRightUpGroup5");
+
+//		tentacleLeftDownGroup1 = serializedObject.FindProperty("tentacleLeftDownGroup1");
+//		tentacleLeftDownGroup2 = serializedObject.FindProperty("tentacleLeftDownGroup2");
+//		tentacleLeftDownGroup3 = serializedObject.FindProperty("tentacleLeftDownGroup3");
+//		tentacleLeftDownGroup4 = serializedObject.FindProperty("tentacleLeftDownGroup4");
+//		tentacleLeftDownGroup5 = serializedObject.FindProperty("tentacleLeftDownGroup5");
+
+//		tentacleRightDownGroup1 = serializedObject.FindProperty("tentacleRightDownGroup1");
+//		tentacleRightDownGroup2 = serializedObject.FindProperty("tentacleRightDownGroup2");
+//		tentacleRightDownGroup3 = serializedObject.FindProperty("tentacleRightDownGroup3");
+//		tentacleRightDownGroup4 = serializedObject.FindProperty("tentacleRightDownGroup4");
+//		tentacleRightDownGroup5 = serializedObject.FindProperty("tentacleRightDownGroup5");
+
+//		maxLeafRotation1 = serializedObject.FindProperty("maxLeafRotation1");
+//		maxLeafRotation2 = serializedObject.FindProperty("maxLeafRotation2");
+//		maxLeafRotation3 = serializedObject.FindProperty("maxLeafRotation3");
+//		maxLeafRotation4 = serializedObject.FindProperty("maxLeafRotation4");
+//		maxLeafRotation5 = serializedObject.FindProperty("maxLeafRotation5");
+
+//		// 花瓣 List
+//		tentacleflowerLeftUpGroup1 = serializedObject.FindProperty("tentacleflowerLeftUpGroup1");
+//		tentacleflowerLeftUpGroup2 = serializedObject.FindProperty("tentacleflowerLeftUpGroup2");
+//		tentacleflowerLeftUpGroup3 = serializedObject.FindProperty("tentacleflowerLeftUpGroup3");
+
+//		tentacleflowerRightUpGroup1 = serializedObject.FindProperty("tentacleflowerRightUpGroup1");
+//		tentacleflowerRightUpGroup2 = serializedObject.FindProperty("tentacleflowerRightUpGroup2");
+//		tentacleflowerRightUpGroup3 = serializedObject.FindProperty("tentacleflowerRightUpGroup3");
+
+//		tentacleflowerLeftDownGroup1 = serializedObject.FindProperty("tentacleflowerLeftDownGroup1");
+//		tentacleflowerLeftDownGroup2 = serializedObject.FindProperty("tentacleflowerLeftDownGroup2");
+//		tentacleflowerLeftDownGroup3 = serializedObject.FindProperty("tentacleflowerLeftDownGroup3");
+
+//		tentacleflowerRightDownGroup1 = serializedObject.FindProperty("tentacleflowerRightDownGroup1");
+//		tentacleflowerRightDownGroup2 = serializedObject.FindProperty("tentacleflowerRightDownGroup2");
+//		tentacleflowerRightDownGroup3 = serializedObject.FindProperty("tentacleflowerRightDownGroup3");
+
+//		maxFlowerRotation1 = serializedObject.FindProperty("maxFlowerRotation1");
+//		maxFlowerRotation2 = serializedObject.FindProperty("maxFlowerRotation2");
+//		maxFlowerRotation3 = serializedObject.FindProperty("maxFlowerRotation3");
+
+//		maxRotation1 = serializedObject.FindProperty("maxRotation1");
+//		maxRotation2 = serializedObject.FindProperty("maxRotation2");
+//		maxRotation3 = serializedObject.FindProperty("maxRotation3");
+	
+//	}
+
+//	public override void OnInspectorGUI()
+//	{
+//		serializedObject.Update();
+
+//		GUILayout.Label("中央眼睛與花瓣基本控制", EditorStyles.boldLabel);
+//		EditorGUILayout.PropertyField(trigger1, new GUIContent("右下眼睛"));
+//		EditorGUILayout.PropertyField(trigger2, new GUIContent("左下眼睛"));
+//		EditorGUILayout.PropertyField(trigger3, new GUIContent("右上眼睛"));
+//		EditorGUILayout.PropertyField(trigger4, new GUIContent("左上眼睛"));
+//		EditorGUILayout.PropertyField(specialTrigger, new GUIContent("中央眼睛"));
+//		EditorGUILayout.PropertyField(newTrigger, new GUIContent("眼睛隱藏"));
+//		EditorGUILayout.PropertyField(outTrigger, new GUIContent("花瓣開合"));
+
+//		GUILayout.Space(10);
+
+//		DrawTentacleSection("左上葉片與花瓣控制", tentacleLeftUptrigger, tentacleflowerLeftUptrigger,
+//			tentacleLeftUpGroup1, tentacleLeftUpGroup2, tentacleLeftUpGroup3, tentacleLeftUpGroup4, tentacleLeftUpGroup5,
+//			tentacleflowerLeftUpGroup1, tentacleflowerLeftUpGroup2, tentacleflowerLeftUpGroup3);
+
+//		DrawTentacleSection("右上葉片與花瓣控制", tentacleRightUptrigger, tentacleflowerRightUptrigger,
+//			tentacleRightUpGroup1, tentacleRightUpGroup2, tentacleRightUpGroup3, tentacleRightUpGroup4, tentacleRightUpGroup5,
+//			tentacleflowerRightUpGroup1, tentacleflowerRightUpGroup2, tentacleflowerRightUpGroup3);
+
+//		DrawTentacleSection("左下葉片與花瓣控制", tentacleLeftDowntrigger, tentacleflowerLeftDowntrigger,
+//			tentacleLeftDownGroup1, tentacleLeftDownGroup2, tentacleLeftDownGroup3, tentacleLeftDownGroup4, tentacleLeftDownGroup5,
+//			tentacleflowerLeftDownGroup1, tentacleflowerLeftDownGroup2, tentacleflowerLeftDownGroup3);
+
+//		DrawTentacleSection("右下葉片與花瓣控制", tentacleRightDowntrigger, tentacleflowerRightDowntrigger,
+//			tentacleRightDownGroup1, tentacleRightDownGroup2, tentacleRightDownGroup3, tentacleRightDownGroup4, tentacleRightDownGroup5,
+//			tentacleflowerRightDownGroup1, tentacleflowerRightDownGroup2, tentacleflowerRightDownGroup3);
+
+//		// --- 最大旋轉值 ---
+//		GUILayout.Space(5);
+//		GUILayout.Label("觸手最大旋轉值", EditorStyles.boldLabel);
+//		EditorGUILayout.PropertyField(maxRotation1, new GUIContent("中央花瓣1_旋轉Max"));
+//		EditorGUILayout.PropertyField(maxRotation2, new GUIContent("中央花瓣2_旋轉Max"));
+//		EditorGUILayout.PropertyField(maxRotation3, new GUIContent("中央花瓣3_旋轉Max"));
+
+//		EditorGUILayout.PropertyField(maxLeafRotation1, new GUIContent("觸手葉片1_旋轉Max"));
+//		EditorGUILayout.PropertyField(maxLeafRotation2, new GUIContent("觸手葉片2_旋轉Max"));
+//		EditorGUILayout.PropertyField(maxLeafRotation3, new GUIContent("觸手葉片3_旋轉Max"));
+//		EditorGUILayout.PropertyField(maxLeafRotation4, new GUIContent("觸手葉片4_旋轉Max"));
+//		EditorGUILayout.PropertyField(maxLeafRotation5, new GUIContent("觸手葉片5_旋轉Max"));
+
+//		EditorGUILayout.PropertyField(maxFlowerRotation1, new GUIContent("觸手花瓣1_旋轉Max"));
+//		EditorGUILayout.PropertyField(maxFlowerRotation2, new GUIContent("觸手花瓣2_旋轉Max"));
+//		EditorGUILayout.PropertyField(maxFlowerRotation3, new GUIContent("觸手花瓣3_旋轉Max"));
+
+
+//		GUILayout.Space(10);
+
+//		GUILayout.Label("眼睛朝向設定", EditorStyles.boldLabel);
+//		EditorGUILayout.PropertyField(Look_Target, new GUIContent("朝向目標"));
+//		EditorGUILayout.PropertyField(target, new GUIContent("中央眼睛目標"));
+//		EditorGUILayout.PropertyField(EyesGroup, true);
+
+//		serializedObject.ApplyModifiedProperties();
+//	}
+
+//	private void DrawTentacleSection(string title, SerializedProperty leafTrigger, SerializedProperty flowerTrigger,
+//		SerializedProperty group1, SerializedProperty group2, SerializedProperty group3, SerializedProperty group4, SerializedProperty group5,
+//		SerializedProperty flowerGroup1, SerializedProperty flowerGroup2, SerializedProperty flowerGroup3)
+//	{
+//		GUILayout.Space(10);
+//		GUILayout.Label(title, EditorStyles.boldLabel);
+//		EditorGUILayout.PropertyField(leafTrigger, new GUIContent("葉片開合滑條"));
+//		EditorGUILayout.PropertyField(group1, new GUIContent("葉片群組1"), true);
+//		EditorGUILayout.PropertyField(group2, new GUIContent("葉片群組2"), true);
+//		EditorGUILayout.PropertyField(group3, new GUIContent("葉片群組3"), true);
+//		EditorGUILayout.PropertyField(group4, new GUIContent("葉片群組4"), true);
+//		EditorGUILayout.PropertyField(group5, new GUIContent("葉片群組5"), true);
+
+//		EditorGUILayout.PropertyField(flowerTrigger, new GUIContent("花瓣開合滑條"));
+//		EditorGUILayout.PropertyField(flowerGroup1, new GUIContent("花瓣群組1"), true);
+//		EditorGUILayout.PropertyField(flowerGroup2, new GUIContent("花瓣群組2"), true);
+//		EditorGUILayout.PropertyField(flowerGroup3, new GUIContent("花瓣群組3"), true);
+//	}
+//}
