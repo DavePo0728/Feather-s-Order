9ddd662b08b679b846e82a3cc1ef6a2a932aec6d
Asuisui <tomsowo@gmail.com>
2025-07-07 18:22:31 +0800

    Asuisui #070701

--- Assets/PlayerMissileMove.cs
+++ Assets/PlayerMissileMove.cs
@@ -61,7 +61,7 @@ public class PlayerMissileMove : MonoBehaviour
 			float result = 1f / BulletCount;
 			float rounded = (float)Math.Round(result, 3);  // 四捨五入到小數點後3位
 			rounded = Mathf.Max(rounded, 0.3f);
-			Debug.Log("Hit enemy: " + rounded + "BulletCount:	" + BulletCount);
+			//Debug.Log("Hit enemy: " + rounded + "BulletCount:	" + BulletCount);
 			other.GetComponent<EnemyHp>().PlayhitMisairuAudio(rounded);
 
 			Destroy(this.gameObject);
